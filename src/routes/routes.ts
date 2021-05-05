5ea5836f085db4b18456f6cd766067e87a06404e
Felipe Oliveira <felipe.boliveira@outlook.com>
2021-05-05 01:26:33 -0300

    Atualiza rotas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,12 +3,16 @@ import { Router } from 'express';
 import { UserController } from '../controllers/user.controller';
 import { SurveysController } from '../controllers/survey.controller';
 import { SendMailController } from '../controllers/sendMail.controller';
+import { AnswerController } from '../controllers/answer.controller';
+import { NpsController } from '../controllers/nps.controller';
 
 const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
+const npsController = new NpsController();
 
 router.post('/users', userController.create);
 
@@ -16,5 +20,9 @@ router.post('/surveys', surveysController.create);
 router.get('/surveys', surveysController.show);
 
 router.post("sendMail", sendMailController.execute);
+
+router.get("answers/:value", answerController.execute);
+
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
