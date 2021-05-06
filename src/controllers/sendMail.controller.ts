29215d0cc9a7e410dcbbfff40e50b05e4ce3c99a
Felipe Barbosa <felipe.barbosa@calindra.com.br>
2021-05-06 18:54:25 -0300

    Adiciona gerenciamento de erros

--- src/controllers/sendMail.controller.ts
+++ src/controllers/sendMail.controller.ts
@@ -6,6 +6,7 @@ import { SurveysRepository } from '../repositories/survey.repository';
 import { UsersRepository } from '../repositories/user.repository';
 import { SurveysUsersRepository } from '../repositories/surveysUsers.repository';
 import sendMailService from '../services/sendMail.service';
+import { AppError } from '../errors/app.error';
 
 
 class SendMailController {
@@ -19,17 +20,13 @@ class SendMailController {
     const user = await usersRepository.findOne({ email });
 
     if (!user) {
-        return response.status(400).json({
-            error: "User does not exists",
-        })
+        throw new AppError("User does not exists")
     }
 
     const survey = await surveysRepository.findOne({ id: survey_id });
 
     if (!survey) {
-        return response.status(400).json({
-            error: "Survey does not exists",
-        })
+        throw new AppError("Survey does not exists!")
     }
 
     // Salvar as informações na tabela surveyUser
